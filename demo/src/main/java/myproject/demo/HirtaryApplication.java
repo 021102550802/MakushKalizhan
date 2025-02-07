10637119177e1dc0f4bd34880c5a5615bb957f1f
021102550802 <242567@astanait.edu.kz>
2025-02-07 06:20:25 +0500

    1

---
+++ demo/src/main/java/myproject/demo/HirtaryApplication.java
@@ -0,0 +1,13 @@
+package myproject.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HirtaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HirtaryApplication.class, args);
+	}
+
+}
